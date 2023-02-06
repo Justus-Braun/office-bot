be743dc68f3deb6117dec8ff3ad53a1f1a8f66ff
Justus Braun <justusbraun03@gmail.com>
2023-02-06 23:07:46 +0100

    Fixed error

--- src/index.js
+++ src/index.js
@@ -16,23 +16,29 @@ setInterval(() => {
 	const guilds = client.guilds.cache;
 	guilds.forEach(async guild => {
 		const pb = new PocketBase(process.env.PB_ENDPOINT);
-		const result = await pb.collection('times').getList(1, 50, {
-			filter: `guild = ${guild.id}`
-		})
 
-		if (result.items.length > 0) {
-			const start = new Date();
-			const end = new Date();
-			start.setHours(result.items[0].startHours);
-			start.setMinutes(result.items[0].startMinutes);
-			end.setHours(result.items[0].endHours);
-			end.setMinutes(result.items[0].endMinutes);
+		try {
+			const result = await pb.collection('times').getList(1, 50, {
+				filter: `guild = ${guild.id}`
+			})
 
-			const now = new Date();
+			if (result.items.length > 0) {
+				const start = new Date();
+				const end = new Date();
+				start.setHours(result.items[0].startHours);
+				start.setMinutes(result.items[0].startMinutes);
+				end.setHours(result.items[0].endHours);
+				end.setMinutes(result.items[0].endMinutes);
 
-			const channel = guild.channels.cache.get(result.items[0].channel_id);
-            const channelName = `${now > start && now < end ? "Open" : "Closed"} : ${start.getHours()}:${(start.getMinutes()<10?'0':'') + start.getMinutes()} - ${end.getHours()}:${(end.getMinutes()<10?'0':'') + end.getMinutes()}`
-			channel.setName(channelName);
+				const now = new Date();
+
+				const channel = guild.channels.cache.get(result.items[0].channel_id);
+				const channelName = `${now > start && now < end ? "Open" : "Closed"} : ${start.getHours()}:${(start.getMinutes()<10?'0':'') + start.getMinutes()} - ${end.getHours()}:${(end.getMinutes()<10?'0':'') + end.getMinutes()}`
+				channel.setName(channelName);
+			}
+		} catch (error) {
+			console.log("error")
+			console.log(error)
 		}
 	})
 }, INTERVAL);
